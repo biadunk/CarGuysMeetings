c243ca017730945aa56b1634026a5bee5f263824
Kacper Biaduń <kacperbiadun.12@gmail.com>
2023-03-06 17:33:21 +0100

    Initial Commit

---
+++ CarGuysMeetings/CarGuysMeetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarGuysMeetingsApp.swift
+//  CarGuysMeetings
+//
+//  Created by Kacper Biaduń on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarGuysMeetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
